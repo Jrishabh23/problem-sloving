49148e5b1fba1a6fcb4e049883d9d885971d69f2
Rishabh Jain <84099807+Jrishabh23@users.noreply.github.com>
2022-02-28 00:49:20 +0530

    Update find-total-number-occurance-in-array.js
    Add proper name variable.

--- find-total-number-occurance-in-array.js
+++ find-total-number-occurance-in-array.js
@@ -1,6 +1,6 @@
-const arr = [2,3,78,2,3,1,2,3,2,3,2,43,5,6,78,9,54,32,45];
-const obj = {};
-for(let num of arr ) {
-  obj[`${num}`] = obj.hasOwnProperty(`${num}`) ? ++obj[`${num}`] : 1;
+const duplicateNumberArray = [2,3,78,2,3,1,2,3,2,3,2,43,5,6,78,9,54,32,45];
+const emptyObject = {};
+for(let value of duplicateNumberArray ) {
+  emptyObject[`${value}`] = emptyObject.hasOwnProperty(`${value}`) ? ++emptyObject[`${value}`] : 1;
 }
-console.log(obj)
+console.log('Array contain unique number', Object.keys(emptyObject))
