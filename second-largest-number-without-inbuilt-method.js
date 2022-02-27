3d6a3a64174ab6cd452a08ec21c794b94ae0408e
Rishabh Jain <84099807+Jrishabh23@users.noreply.github.com>
2022-02-28 00:32:33 +0530

    Update second-largest-number-without-inbuilt-method.js
    Add console.log for print answer.

--- second-largest-number-without-inbuilt-method.js
+++ second-largest-number-without-inbuilt-method.js
@@ -4,23 +4,21 @@
 **/
 const arr = [2,3,43,5,6,78,9,54,32,45];
 if(arr.length <= 2) {
-  return "Array have minimum two value";
+  console.log("Array have minimum two value");
 }
 for(let num of arr ) {
   arr[1] = num > arr[1] && num > arr[0] ? arr[0] : (num > arr[1] ? num : arr[1]) ;
   arr[0] = num > arr[0] ? num : arr[0];
 }
-
-
-console.log('a is first largest',a,'b is second largest number',b)
+console.log('a is first largest',arr[0],'b is second largest number',arr[1])
 
 /**
-* first solution
+* second solution
 **/
 
 const arr = [2,3,43,5,6,78,9,54,32,45];
 if(arr.length <= 2) {
-  return 'Array have minimum two value";
+  conosle.log('Array have minimum two value");
 }
 arr[0] < arr[1] ? 
 [arr[0], arr[1]] = [arr[1], arr[0]] : '';
@@ -29,3 +27,4 @@ for(let num of arr ) {
   b = num > b && num > a ? a : (num > b ?num : b) ;
   a = num > a ? num : a;
 }
+console.log('a is first largest',a,'b is second largest number',b)
